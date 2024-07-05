6516212e8473372c1b87f9cf2e919d85760f51f4
Vedantnawale <vedantnawale6@gmail.com>
2024-07-05 12:32:34 +0530

    useEffect and ReactRouter

---
+++ src/components/PokemonList/PokemonList.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
+import './PokemonList.css'
+import { useState } from 'react'
+import Pokemon from '../Pokemon/Pokemon'
+
+const PokemonList = () => {
+
+    const [pokemonList, setPokemonList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const [pokedexUrl, setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon/')
+
+    const [nextUrl, setNextUrl] = useState('');
+    const [prevUrl, setPrevUrl] = useState('')
+
+    async function downloadPokemons() {
+        const response = await axios.get(pokedexUrl); // this downloads list of 20 pokemons
+        console.log(response);
+
+        const pokemonResults = response.data.results; // we get the array of pokemons from result
+        console.log(pokemonResults);
+
+        // setNextUrl(response.data.next)
+        // setPrevUrl(response.data.previous)
+
+        // iterating over the array of pokemons, and using their url, to create an array of promises
+        // that will download those 20 pokemons
+        const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url));
+
+        console.log(pokemonResultPromise);
+
+        // passing that promise array to axios.all
+        const pokemonData = await axios.all(pokemonResultPromise); // array of 20 pokemon detailed data 
+        // console.log(pokemonData);
+
+        // now iterate on the data of each pokemon, and extract id, name, image, types
+        const pokeListResult = pokemonData.map((pokeData) => {
+            const pokemon = pokeData.data
+            return {
+                id: pokemon.id,
+                name: pokemon.name,
+                image: (pokemon.sprites.other) ? pokemon.sprites.other.dream_world.front_default : pokemon.sprites.front_shiny,
+                types: pokemon.types
+            }
+
+        })
+        // console.log(pokeListResult);
+        setPokemonList(pokeListResult)
+        setIsLoading(false)
+    }
+
+
+    useEffect(() => {
+        setIsLoading(true)
+        downloadPokemons();
+    }, [pokedexUrl])
+
+    return (
+
+        <div className='pokemon-list-wrapper'>
+            <div className='pokemon-wrapper'>
+                {
+                    (isLoading) ? 'Loading...' :
+                        pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} id={p.id} />)
+                }
+            </div>
+
+            <div className='controls'>
+                <button disabled={prevUrl == null} onClick={() => setPokedexUrl(prevUrl)}>Prev</button>
+                <button disabled={nextUrl == null} onClick={()=>setPokedexUrl(nextUrl)}>Next</button>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default PokemonList
+
+// useEffect tyachya state var depend karte 1> [] --> pahilya vedes 2> [x] --> jevha x change hoil 3> [x, y] --> ya don paiki kontahi ek change
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Search.css'
+
+const Search = () => {
+    return (
+        <div className='search-wrapper'>
+            <input
+                id='pokemon-name-search'
+                type='text'
+                placeholder='pokemon name...'
+            />
+        </div>
+    )
+}
+
+export default Search
